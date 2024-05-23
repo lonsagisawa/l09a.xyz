50d68a938afcae3f4e66922752ba7072707fff0c
Lon Sagisawa <lon@sagisawa.me>
2024-05-24 03:30:39 +0900

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '@fontsource/ibm-plex-mono';
+	import '@fontsource/ibm-plex-mono/400-italic.css';
+	import '../app.css';
+</script>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<main class="h-screen flex flex-col justify-center items-center gap-8 text-slate-600">
+	<h1 class="text-6xl font-sans">L09a<br /><span class="italic">.xyz</span></h1>
+	<div>
+		<ul>
+			<li>
+				<a href="//hatsuboshi.l09a.xyz" class="text-slate-400 hover:text-orange-400 underline"
+					>Hatsuboshi Tools</a
+				>
+			</li>
+		</ul>
+	</div>
+</main>
